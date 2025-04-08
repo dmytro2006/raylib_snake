df7be51b5b2ef9bb582e5430c36fe42342d4bbe1
dmytro2006 <garasyuk2006@gmail.com>
2025-04-08 08:31:42 +0200

    Add direction.h

---
+++ include/direction.h
@@ -0,0 +1,16 @@
+//
+// Created by dmytr on 04/04/2025.
+//
+
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum class Direction
+{
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+};
+
+#endif //DIRECTION_H
